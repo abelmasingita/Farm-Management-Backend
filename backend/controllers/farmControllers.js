79a15d1be43e22341f90c3b5849a827e5603209f
abel masingita <60102940+abelmasingita@users.noreply.github.com>
2024-02-26 10:05:38 +0200

    Farm Crud

--- backend/controllers/farmControllers.js
+++ backend/controllers/farmControllers.js
@@ -13,4 +13,69 @@ const getFarms = asyncHandler(async (req, res) => {
   }
 })
 
-export { getFarms }
+const getFarmId = asyncHandler(async (req, res) => {
+  const farm = await Farm.findById(req.params.id)
+
+  if (farm) {
+    res.status(200).json(farm)
+  } else {
+    res.status(404)
+    throw new Error('Farm not found!')
+  }
+})
+
+const deleteFarm = asyncHandler(async (req, res) => {
+  const farm = await Farm.findById(req.params.id)
+
+  if (farm) {
+    await Farm.deleteOne({ _id: req.params.id })
+    res.status(200).json(`Farm Deleted!`)
+  } else {
+    res.status(404)
+    throw new Error('Farm Not Found!')
+  }
+})
+
+const createFarm = asyncHandler(async (req, res) => {
+  const { name, location, size, owner, contactInformation } = req.body
+
+  try {
+    const farm = await Farm.create({
+      name: name,
+      location: location,
+      size: size,
+      owner: owner,
+      contactInformation: contactInformation,
+    })
+
+    if (farm) {
+      res.status(201)
+      res.json(farm)
+    } else {
+      throw new Error('Farm not created!')
+    }
+  } catch (error) {
+    console.log(`Error ${error.message}`)
+  }
+})
+
+const updateFarm = asyncHandler(async (req, res) => {
+  const { name, location, size, owner, contactInformation } = req.body
+
+  const farm = await Farm.findById(req.params.id)
+
+  if (farm) {
+    ;(farm.name = name),
+      (farm.size = size),
+      (farm.location = location),
+      (farm.owner = owner),
+      (farm.contactInformation = contactInformation)
+
+    const updatedFarm = await farm.save()
+    res.json(updatedFarm)
+  } else {
+    res.status(404)
+    throw new Error('Farm not found')
+  }
+})
+export { getFarms, getFarmId, deleteFarm, createFarm, updateFarm }
--- backend/routes/farmRoutes.js
+++ backend/routes/farmRoutes.js
@@ -1,8 +1,16 @@
 import express from 'express'
-import { getFarms } from '../controllers/farmControllers.js'
+import {
+  createFarm,
+  deleteFarm,
+  getFarmId,
+  getFarms,
+  updateFarm,
+} from '../controllers/farmControllers.js'
 
 const router = express.Router()
 
-router.route('/').get(getFarms)
+router.route('/').get(getFarms).post(createFarm)
+
+router.route('/:id').get(getFarmId).delete(deleteFarm).put(updateFarm)
 
 export default router
